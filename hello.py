f0674e183620315a4fc3342557ed79412041afb1
Medisetty Venkata Raviteja <ravitejaomini9963@gmail.com>
2026-05-03 18:47:38 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,8 @@
+# Python Syntax and indentation
+#  *Syntax is the correct way of writing code in Python.
+#  *Use 4 spaces or 1 tab to indent code that belongs to a block.
+#  *Python will give an error if indentation is missing or not correct.
+
+# Eg;-
+if 5 > 3:
+    print("Five is greater")  
